c97bb33e3ffcf7aea20e837fed57bed17521bd80
dhruv <dhruv18ranjan@gmail.com>
2023-01-31 17:31:20 +0530

    integration

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getHotel,
@@ -16,13 +18,16 @@ router.post("/", verifyAdmin, createHotel);
 //UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", verifyAdmin, deleteHotel);
+router.delete("/find/:id", verifyAdmin, deleteHotel);
 //GET
 
 router.get("/find/:id", getHotel);
 //GET ALL
 
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+
 
 
 export default router;
--- frontend/src/components/propertyList/PropertyList.jsx
+++ frontend/src/components/propertyList/PropertyList.jsx
@@ -1,8 +1,14 @@
+import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
+  const {data,loading,error} = useFetch("/hotels/countByType");
+
   return (
     <div className="pList">
+    { loading? "loading" :
+      <>
+
       <div className="pListItem">
         <img
           src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
@@ -58,6 +64,8 @@ const PropertyList = () => {
           <h2>2331 hotels</h2>
         </div>
       </div>
+      </>
+    }
     </div>
   );
 };
